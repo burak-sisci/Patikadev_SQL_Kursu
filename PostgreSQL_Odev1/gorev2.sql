1a571ec36bec02e9276e0e3c30fc65e532141d3a
Burak ŞİŞCİ <buraksisci32@gmail.com>
2025-05-14 13:05:37 +0300

    Add files via upload

---
+++ PostgreSQL_Odev1/gorev2.sql
@@ -0,0 +1,4 @@
+--film tablosunda bulunan tüm sütunlardaki verileri film uzunluğu (length) 60 dan büyük VE 75 ten küçük olma koşullarıyla sıralayınız.
+select * from film
+where length <= 75
+and length >= 60
